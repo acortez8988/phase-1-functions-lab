596b54334459dcdeed4c582532e3b69663bee6ad
Alexander Cortez <acortez8988@gmail.com>
2022-07-06 12:36:05 -0400

    commit tree 22109ab2e59d2a92f5862ef52183e26f832690f6

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickUpLocation) {
+    
+    return Math.abs(42 - pickUpLocation ); 
+  }
+
+  function distanceFromHqInFeet(pickUpLocation) {
+    return 264 * distanceFromHqInBlocks(pickUpLocation);
+  }
+
+  function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination)*264;
+  }
+
+  function calculatesFarePrice(start, destination) {
+    let distanceInFeet = distanceTravelledInFeet (start, destination);
+    
+   if(distanceInFeet <= 400){
+    return 0;
+   } 
+   else if(distanceInFeet > 400 && distanceInFeet <= 2000){
+    const result = distanceInFeet - 400
+           return result * 0.02;
+   }
+   else if(distanceInFeet > 2000 && distanceInFeet <= 2500){
+     return 25;
+   }
+   else if( distanceInFeet > 2500) 
+   return `cannot travel that far`
+}
+  
